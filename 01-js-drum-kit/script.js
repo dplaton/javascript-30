c6702bf4f7e2f4ecd3259d3d8336bd684f7af001
Daniel Platon <platon@adobe.com>
2018-11-11 23:34:28 +0200

    [01-js-drumkit] Fix minor formatting issues

--- 01-js-drum-kit/script.js
+++ 01-js-drum-kit/script.js
@@ -1,28 +1,34 @@
-window.addEventListener('keydown', (event) => {
-    const keyCode = event.keyCode;
+(function () {
 
-    let key = document.querySelector(`.key[data-key="${keyCode}"]`);
-    let audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    function playSound(event) {
+        const keyCode = event.keyCode;
 
-    if (!key || !audio) {
-        return;
+        let key = document.querySelector(`.key[data-key="${keyCode}"]`);
+        let audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+
+        if (!key || !audio) {
+            return;
+        }
+
+        key
+            .classList
+            .add('playing');
+        audio.currentTime = 0;
+        audio.play();
     }
 
-    key
-        .classList
-        .add('playing');
-    audio.currentTime = 0;
-    audio.play();
-});
+    function removeTransition(elem) {
+        if (elem.propertyName !== 'transform') 
+            return;
+        this
+            .classList
+            .remove('playing');
+    }
 
-const keys = document.querySelectorAll('.key');
-function removeTransition(elem) {
-    if (elem.propertyName !== 'transform') 
-        return;
-    this
-        .classList
-        .remove('playing');
-}
-keys.forEach(key => {
-    key.addEventListener('transitionend', removeTransition);
-})
+    window.addEventListener('keydown', playSound);
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => {
+        key.addEventListener('transitionend', removeTransition);
+    })
+})();
